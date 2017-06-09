a85e26a94af48293238546f8b2c21e1879c55f25
ABZsto <ABZstudio2017@gmail.com>
2017-06-09 17:06:07 +0700

    Create sdfs.php

--- sdfs.php
+++ sdfs.php
@@ -29,9 +29,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'event' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = "สวัสดี"//$event['message']['text'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
