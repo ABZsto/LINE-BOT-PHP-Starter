80f414d46c29a28ed7ac2cff303a9d4f52b26be0
ABZsto <ABZstudio2017@gmail.com>
2017-06-09 17:40:29 +0700

    Create sdfs.php

--- sdfs.php
+++ sdfs.php
@@ -2,12 +2,12 @@
 {
   "events": [
       {
-        "replyToken": "u8b4544c027253be2993f7b87f058124c",
+        "replyToken": "Uc256b7e100b0ad95ecc9140fea984eae",
         "type": "message",
         "timestamp": 1519190751,
         "source": {
              "type": "user",
-             "userId": "u8b4544c027253be2993f7b87f058124c"
+             "userId": "Uc256b7e100b0ad95ecc9140fea984eae"
          },
          "message": {
              "id": "1519190751",
