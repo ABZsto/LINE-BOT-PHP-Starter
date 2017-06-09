ae2e224582b9fcdf870f9796ec0ea7424114eb17
ABZsto <ABZstudio2017@gmail.com>
2017-06-09 16:23:52 +0700

    Create sdfs.php

--- sdfs.php
+++ sdfs.php
@@ -1,5 +1,5 @@
 <?php
 curl -X POST 
 -H 'Content-Type:application/json' 
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' 
+-H 'Authorization: Bearer K1IWohnjcPI3OZ9l4IeYz9YT5plWDTv9g9VmPN08PdJD7pbyv+GRzVeI7zl4WVlpf/DAcu2q/nJqh6IAXS9JYER5MHAhX4RtDyKgg8AOfPdd7hgK+rKQxLk8ZjkMrZJ8SCTZf/MORouXmLm++jdH8gdB04t89/1O/w1cDnyilFU=' 
 echo "I am a bot";
