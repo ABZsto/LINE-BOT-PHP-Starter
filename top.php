598e1810160c17545fa8159ff52d4b17bbb2a869
ABZsto <ABZstudio2017@gmail.com>
2017-06-12 12:09:12 +0700

    Create top.php

--- top.php
+++ top.php
@@ -15,7 +15,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี คุณ ".$arrJson['events'][0]['text']['user'];
+  $arrPostData['messages'][0]['text'] = "สวัสดีค่ะ";
  
 }
 else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
