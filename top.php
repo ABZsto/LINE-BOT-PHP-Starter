c07323de69cd056fb6413cdba59ab692baf0d248
ABZsto <ABZstudio2017@gmail.com>
2017-06-12 12:45:07 +0700

    Create top.php

--- top.php
+++ top.php
@@ -30,6 +30,12 @@ else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+}
+  else if($arrJson[[:digit:]]+\.?[[:digit:]]*,['events'][0]['message']['text'] == "ได้เท่าไหร่"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "is_integer()";
+  $arrPostData['messages'][0]['text'] = "ได้";
  
 }
 else{
