f7dacc249cbf77dae15f59d46566940b2e39fec4
ABZsto <ABZstudio2017@gmail.com>
2017-06-12 16:44:50 +0700

    Create top.php

--- top.php
+++ top.php
@@ -45,12 +45,13 @@ else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 }
 else if($arrJson['events'][0]['message']['text'] == "12ss"){
-      foreach ($arrJson as $element) {
+  $arr = str_split($arrJson);
+      foreach ($arr as $element) {
         if (is_numeric($element)) {
            $arrPostData = array();
            $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
            $arrPostData['messages'][0]['type'] = "text";
-           $arrPostData['messages'][0]['text'] = "".($element);
+           $arrPostData['messages'][0]['text'] = "ได้แล้ว";
         }
  
  
