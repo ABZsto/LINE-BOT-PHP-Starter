e1b5ab4a695ff8ecd1fd67bc393d7a87e1b04d11
ABZsto <ABZstudio2017@gmail.com>
2017-06-13 10:58:22 +0700

    Create top.php

--- top.php
+++ top.php
@@ -44,11 +44,13 @@ else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
  
-}else if($arrJson['events'][0]['message']['sticker'] == 'sticker'){
+}
+else if($arrJson['events'][0]['message']['text'] == "ส่ง"){
   $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "sticker";
-  $arrPostData['messages'][0]['packageId']="1";
-  $arrPostData['messages'][0]['stickerId'] ="1";
+  $arrPostData['messages'][0]['packageId'] = "1";
+  $arrPostData['messages'][0]['stickerId'] = "1";
   
 }
 //else if($arrJson['events'][0]['message']['text']){
