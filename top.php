8559b9aa255eca79b75d81ba08f7b169371fea20
ABZsto <ABZstudio2017@gmail.com>
2017-06-13 11:34:41 +0700

    ๅ

--- top.php
+++ top.php
@@ -57,8 +57,8 @@ else if($arrJson['events'][0]['message']['text'] == "image"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "image";
-  $arrPostData['messages'][0]['originalContentUrl'] = "https://example.com/original.jpg";
-  $arrPostData['messages'][0]['previewImageUrl'] = "https://example.com/preview.jpg";
+  $arrPostData['messages'][0]['originalContentUrl'] = "https://f.ptcdn.info/443/047/000/oh4t51mx8k5Q5KoJ14p-o.jpg";
+  $arrPostData['messages'][0]['previewImageUrl'] = "https://f.ptcdn.info/443/047/000/oh4t51mx8k5Q5KoJ14p-o.jpg";
   }
 //else if($arrJson['events'][0]['message']['text']){
  // $arr = str_split($arrJson['events'][0]['message']['text']);
